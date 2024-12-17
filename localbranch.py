9b64d63dbf70b58a5f97f0c06173068677c8e7e4
Dilip Sing <singdilip112@gmail.com>
2024-12-17 22:28:54 +0530

    Adding calculator code and unittest

--- localbranch.py
+++ localbranch.py
@@ -1,1 +1,17 @@
-print("Hello World !!!")
+# calculator.py
+
+class Calculator:
+    def add(self, x, y):
+        return x + y
+
+    def subtract(self, x, y):
+        return x - y
+
+    def multiply(self, x, y):
+        return x * y
+
+    def divide(self, x, y):
+        if y != 0:
+            return x / y
+        else:
+            return "Error! Division by zero."
